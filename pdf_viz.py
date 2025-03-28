ba6ef343bb24a92b08c72e294274d72d406c43f0
stribus <paladinotoldo@gmail.com>
2025-03-27 22:28:06 -0300

    Adicionar áreas de tabela e colunas específicas para extração de dados no script pdf_viz.py

--- pdf_viz.py
+++ pdf_viz.py
@@ -11,6 +11,9 @@ path = os.path.join(os.getcwd(), f"downloads/pdf/{file_name}.pdf")
 tables = camelot.read_pdf(path
                         , pages='all'
                         ,flavor='stream'
+                        #,flavor='lattice'                        
+                        ,table_areas=['65, 558, 518, 300']
+                        ,columns=['107,156,212,280,336,383,450']
                         )
 
 print(tables[0].parsing_report)
@@ -20,6 +23,10 @@ camelot.plot(tables[0]
             , kind='contour'
             #, kind='grid'
             ).show()
+
+df = tables[0].df
+
+print(df)
 
 print("press any key to continue")
 input()
